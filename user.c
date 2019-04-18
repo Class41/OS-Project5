d61dc8576ca9cfe466e1ec85c6f028f804eb3309
class41 <projectclass41@gmail.com>
2019-04-17 21:03:49 -0500

    mend

--- user.c
+++ user.c
@@ -231,7 +231,7 @@ int main(int argc, int argv)
 				do
 				{
 					resToRequest = (resToRequest + 1) % 21;
-				} while (data->alloc[FindPID(pid)][resToRequest] > 0);
+				} while (data->alloc[resToRequest][FindPID(pid)] > 0);
 
 				srand(time(NULL) ^ (pid << 16));
 				data->req[resToRequest][FindPID(pid)] = (rand() % (data->resVec[resToRequest] - 1));
